88d2307b1b03b28765e4996a68ee684ffef3ecce
teamparmjeet <teamparmjeet@gmail.com>
2025-05-14 10:52:52 +0530

    new

--- app/superadmin/(page)/ChangePassword/page.jsx
+++ app/superadmin/(page)/ChangePassword/page.jsx
@@ -2,19 +2,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 
 export default function ChangePasswordPage() {
-  const { data: session, update } = useSession();
-  const email = session?.user?.email || "";
+  // const { data: session, update } = useSession();
+  // const email = session?.user?.email || "";
   const [formData, setFormData] = useState({
-    email: email,
+    email: "",
     otp: "",
     password: "",
     confirmPassword: "",
   });
 
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(3);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (e) => {
@@ -32,7 +32,7 @@ export default function ChangePasswordPage() {
     try {
       const response = await axios.post("/api/otp", { email: formData.email });
       toast.success(response.data.message);
-      setStep(2);
+      // setStep(2);
     } catch (error) {
       toast.error(error.response?.data?.message || "Something went wrong");
     } finally {
@@ -51,7 +51,7 @@ export default function ChangePasswordPage() {
     try {
       const response = await axios.post("/api/otp", { email: formData.email, otp: formData.otp });
       toast.success(response.data.message);
-      setStep(3);
+      // setStep(3);
     } catch (error) {
       toast.error(error.response?.data?.message || "Invalid OTP");
     } finally {
@@ -76,12 +76,12 @@ export default function ChangePasswordPage() {
     }
     setIsSubmitting(true);
     try {
-      const response = await axios.patch("/api/user/update-user", {
-        id: session?.user?.id,
+      const response = await axios.patch("/api/user/changepasswordbyadmin", {
+        email: formData.email,
         password: formData.password,
       });
       toast.success(response.data.message);
-      setStep(1);
+      // setStep(1);
       setFormData({ email: "", otp: "", password: "", confirmPassword: "" });
     } catch (error) {
       toast.error(error.response?.data?.message || "Failed to update password.");
@@ -94,13 +94,13 @@ export default function ChangePasswordPage() {
    <div className="flex justify-center items-center w-full min-h-screen">
      <div className="max-w-md sm:min-w-md p-6 bg-white dark:bg-gray-700 shadow-lg rounded-lg text-gray-700 dark:text-white">
       <Toaster />
-      <h2 className="text-2xl font-semibold mb-4 text-center">
+      {/* <h2 className="text-2xl font-semibold mb-4 text-center">
         {step === 1 && "Verify Email"}
         {step === 2 && "Enter OTP"}
         {step === 3 && "Reset Password"}
-      </h2>
+      </h2> */}
 
-      {step === 1 && (
+      {/* {step === 1 && (
         <form onSubmit={handleEmailSubmit} className="space-y-4">
           <div>
             <label className="block text-sm font-medium">Email</label>
@@ -146,11 +146,23 @@ export default function ChangePasswordPage() {
             {isSubmitting ? "Verifying..." : "Verify OTP"}
           </button>
         </form>
-      )}
+      )} */}
 
       {step === 3 && (
         <form onSubmit={handlePasswordUpdate} className="space-y-4">
-          <div>
+          <div className="space-y-4">
+               <div>
+            <label className="block text-sm font-medium">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="border rounded-lg p-2 w-full bg-white dark:bg-gray-700 text-gray-700 dark:text-white"
+              required
+              disabled={isSubmitting}
+            />
+          </div>
             <label className="block text-sm font-medium">New Password</label>
             <input
               type="password"
